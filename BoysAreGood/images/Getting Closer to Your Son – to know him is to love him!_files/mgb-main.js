c771ae9580ace9c535d2410c31401166d1b758f0
trgolden3 <52388538+trgolden3@users.noreply.github.com>
2023-07-15 20:07:34 -0400

    Add files via upload
    first effort with DW

---
+++ BoysAreGood/images/Getting Closer to Your Son – to know him is to love him!_files/mgb-main.js
@@ -0,0 +1,11 @@
+jQuery(document).ready(function() {
+  var $grid = jQuery('.grid').masonry({
+    // options...
+    itemSelector: '.grid-item',
+    gutter: 20
+  });
+
+  $grid.imagesLoaded().progress( function() {
+    $grid.masonry('layout');
+  });
+});
